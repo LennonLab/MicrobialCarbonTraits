a8fde363c2f934dbae7cb4bbccb712b1990ce448
Mario Muscarella <mmuscare@indiana.edu>
2015-08-04 21:20:28 -0400

    removed accidental files

--- data/Phylogeny/sina.neighbors_files/ext-silva-settings.js
+++
@@ -1,3 +0,0 @@
-Ext.chart.Chart.CHART_URL = 'typo3conf/ext/pm_tree/pi1/include/extjs/resources/charts.swf';
-Ext.BLANK_IMAGE_URL = 'typo3conf/ext/pm_tree/pi1/include/images/default/s.gif';
-Ext.scopeResetCSS = true;
